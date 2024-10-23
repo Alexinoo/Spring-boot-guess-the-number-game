cf6add6d400d1527ae4e0402924efbd714ebce6a
Alex <alex@stl-horizon.com>
2024-10-23 16:17:51 +0300

    Thymeleaf setup and Initial Template

---
+++ guess-the-number-game/web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming.util;
+
+public final class ViewNames {
+
+    // constant
+    public static final String HOME = "home";
+
+    // constructor
+
+    private ViewNames(){}
+}

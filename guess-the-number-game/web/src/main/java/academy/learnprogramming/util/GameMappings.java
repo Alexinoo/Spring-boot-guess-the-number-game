1e7f4a4c166bb721cacd606a0b9c5766720a9d7e
Alex Mwangi <alex@stl-horizon.com>
2024-10-24 22:14:05 +0300

    Thymeleaf Template Challenge

--- guess-the-number-game/web/src/main/java/academy/learnprogramming/util/GameMappings.java
+++ guess-the-number-game/web/src/main/java/academy/learnprogramming/util/GameMappings.java
@@ -4,5 +4,6 @@ public final class GameMappings {
 
     public static final String PLAY = "play";
     public static final String RESTART = "restart";
+    public static final String Home = "home";
     public static final String REDIRECT_PLAY = "redirect:/"+ PLAY;
 }

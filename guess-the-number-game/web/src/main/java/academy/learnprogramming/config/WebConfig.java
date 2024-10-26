97ab2f7ed693dfdf9cf8292b1c1732cd09bdaf60
Alex Mwangi <alex@stl-horizon.com>
2024-10-26 12:09:44 +0300

    Local Change Interceptor

--- guess-the-number-game/web/src/main/java/academy/learnprogramming/config/WebConfig.java
+++ guess-the-number-game/web/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -2,13 +2,23 @@ package academy.learnprogramming.config;
 
 import academy.learnprogramming.interceptor.RequestInterceptor;
 import academy.learnprogramming.util.ViewNames;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    // bean
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new SessionLocaleResolver();
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -17,7 +27,17 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-       // WebMvcConfigurer.super.addInterceptors(registry);
+       // WebMvcConfigurer.super.addInterceptors(registry); --default
         registry.addInterceptor(new RequestInterceptor());
+
+        /* Setting parameter - http://localhost:8080?lang=es
+        LocaleChangeInterceptor localeChangeInterceptor =
+                new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        registry.addInterceptor(localeChangeInterceptor); */
+
+        /* Using default locale parameter - http://localhost:8080?locale=es */
+        registry.addInterceptor(new LocaleChangeInterceptor());
+
     }
 }
